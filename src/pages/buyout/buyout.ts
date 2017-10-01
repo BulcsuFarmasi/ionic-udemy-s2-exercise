cb22f02afb55484b4a9e3eb41400c62f13ded0ec
Bulcsú Farmasi <bulcsu.farmasi@gmail.com>
2017-10-01 19:02:55 +0200

    add buyout page

---
+++ src/pages/buyout/buyout.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+import { NavController, NavParams } from 'ionic-angular';
+
+@Component({
+  selector: 'page-buyout',
+  templateUrl: 'buyout.html',
+})
+export class BuyoutPage {
+
+  constructor(private navController: NavController, private navParams: NavParams) {}
+
+}
